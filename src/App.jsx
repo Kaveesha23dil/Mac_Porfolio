d43a96a52eb15295ff52ccbb569f470c293d672d
Kaveesha Dilshan <kaveeshadilshankd23@gmail.com>
2025-11-29 22:19:21 +0530

    Full Mac_portfolio

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Navbar, Welcome, Dock } from "./components";
+import gsap from "gsap";
+
+import React from "react";
+import { Draggable } from "gsap/Draggable";
+import { Terminal } from "./windows";
+gsap.registerPlugin(Draggable)
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Welcome />
+      <Dock />
+
+      <Terminal />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Navbar from "./Navbar";
+import Welcome from "./Welcome";
+import Dock from "./Dock";
+import WindowController from "./WindowController";
+
+export { Navbar, Welcome, Dock, WindowController}
